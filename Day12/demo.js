eaca72cee83a2501d7510f7d0521f7c94f55be4d
kushdarji-5 <108213584+kushdarji-5@users.noreply.github.com>
2022-07-26 09:33:24 +0530

    Add files via upload

---
+++ Day12/demo.js
@@ -0,0 +1,24 @@
+function externalAlert()
+{	 
+	alert("externalAlert");
+}
+function externalconfirm()
+{
+	if(confirm("externalConfirm"))
+	{
+		alert("yesss!");
+	
+	else}
+		alert("Nooo!");
+		}
+}	
+function externalprompt()
+{
+	var name = prompt("Enter external prompt value");
+	
+	alert("hiii"+ name);
+	Document.write("text");
+	console.log("text");
+	console.error("text");
+}	
+	
---
+++ Day13/HOISTING.js
@@ -0,0 +1,13 @@
+var a=10;
+
+var b=15;
+function msg(n)
+{
+	let c=20;
+	console.log(a);
+	console.log(b);
+	console.log(c);
+	console.log("hello");
+}
+msg()
+a=10;
